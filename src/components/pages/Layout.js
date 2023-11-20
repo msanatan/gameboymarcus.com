7da381dc3855ce31063ec7bfb7a70e931155cddf
Marcus Sanatan <msanatan@gmail.com>
2023-11-20 17:22:23 -0500

    Add basic layout component
    It's in dire need of styling, I'll work on it

---
+++ src/components/pages/Layout.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Layout.css"; // Import your CSS styles
+
+const Layout = ({ children }) => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <Navbar bg="light" expand="lg" sticky="top">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>GameBoyMarcus</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/contact">
+                <Nav.Link>Contact</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/privacy-policy">
+                <Nav.Link>Privacy Policy</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container fluid className="content">
+        {children}
+      </Container>
+
+      <footer className="footer">
+        <Container>
+          <span>&copy; {currentYear} GameBoyMarcus. All Rights Reserved.</span>
+        </Container>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
