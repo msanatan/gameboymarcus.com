11f5ce84bb5c46c4d7204f63e216b5da4451a955
Marcus Sanatan <msanatan@gmail.com>
2023-12-02 17:10:54 -0400

    Add a basic card
    Needs and image, and will ultimately be loaded from JSON

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,25 @@
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Home = () => {
-  return <></>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card style={{ width: '24rem' }}>
+            <Card.Body>
+              <Card.Title>Star Runner</Card.Title>
+              <Card.Text>
+                Take on the stars and all the enemies that come with it!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
