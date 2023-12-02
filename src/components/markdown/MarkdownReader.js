37c307c6e7f7bef2814ed41cb0ca58281fdaba88
Marcus Sanatan <msanatan@gmail.com>
2023-12-02 17:00:12 -0400

    Create a component for Markdown reading
    Less code duplication + we might want to do cool things on the about page.

---
+++ src/components/markdown/MarkdownReader.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { marked } from "marked";
+import { Container } from "react-bootstrap";
+
+const MarkdownReader = ({ markdownContent }) => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetch(markdownContent)
+      .then((response) => response.text())
+      .then((text) => setContent(marked(text)));
+  }, []);
+
+  return <Container dangerouslySetInnerHTML={{ __html: content }} />;
+}
+
+export default MarkdownReader;
