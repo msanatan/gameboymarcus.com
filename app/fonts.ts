f8829fb2334dd935a22cd3afda2e66ec2af4a0be
Marcus Sanatan <msanatan@gmail.com>
2024-09-11 22:10:29 -0400

    Set up fonts for use wtih Chakra UI

---
+++ app/fonts.ts
@@ -0,0 +1,10 @@
+import localFont from "next/font/local";
+
+export const pressStart2P = localFont({
+  src: "./fonts/PressStart2P-Regular.ttf",
+});
+
+// Font files can be colocated inside of `pages`
+export const arcadeGamer = localFont({
+  src: "./fonts/ArcadeGamer-Regular.ttf",
+});
