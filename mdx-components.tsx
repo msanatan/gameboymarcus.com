871a5d161cdb57e59f12e3ac9ad441c2e79c486f
Marcus Sanatan <msanatan@gmail.com>
2024-09-16 16:55:42 -0400

    Reorg code so it's alphabetical

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -9,7 +9,7 @@ import {
 } from "@chakra-ui/react";
 import type { MDXComponents } from "mdx/types";
 import Link from "next/link";
-import Image, { ImageProps } from "next/image";
+import Image from "next/image";
 import path from "path";
 const { promisify } = require("util");
 import sizeOf from "image-size";
@@ -34,7 +34,68 @@ async function getImageDimensions(src: string) {
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    // Allows customizing built-in components, e.g. to add styling.
+    a: ({ href, children }) => {
+      if (href) {
+        if (href.startsWith("/")) {
+          return (
+            <Link href={href}>
+              <Text as="span" textDecoration="underline">
+                {children}
+              </Text>
+            </Link>
+          );
+        }
+
+        return (
+          <Link href={href} target="_blank" rel="nofollow noopener noreferrer">
+            <Text as="span" textDecoration="underline">
+              {children}
+            </Text>
+          </Link>
+        );
+      }
+    },
+    blockquote: ({ children }) => (
+      <Box
+        as="blockquote"
+        borderLeft="4px solid #00cdac"
+        pl={4}
+        py={2}
+        my={[2, 4]}
+        lineHeight={1.4}
+        bg="#f5f5f5"
+        fontStyle="italic"
+        color="#212529"
+      >
+        {children}
+      </Box>
+    ),
+    code: ({ children, className, ...props }) => {
+      const isInline = !className;
+
+      if (isInline) {
+        return (
+          <Code
+            backgroundColor="black"
+            borderRadius={["lg"]}
+            fontSize={["xs", "sm"]}
+            color="#79c0ff"
+            paddingX={[1, 2, 3]}
+            paddingY={1}
+            fontFamily="pressStart2P"
+            {...props}
+          >
+            {children}
+          </Code>
+        );
+      }
+
+      return (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
     h1: ({ children }) => (
       <Heading
         as="h1"
@@ -68,6 +129,43 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {children}
       </Heading>
     ),
+    img: async ({ src = "", alt = "", ...props }) => {
+      const imageSrc = src.startsWith("/images/posts/")
+        ? src
+        : `/images/posts/${src}`;
+      const { width, height } = await getImageDimensions(imageSrc);
+
+      return (
+        <Image
+          src={imageSrc}
+          alt={alt}
+          width={width}
+          height={height}
+          {...props}
+        />
+      );
+    },
+    li: ({ children }) => (
+      <ListItem
+        as="li"
+        fontSize={["xs", "sm"]}
+        color="black"
+        fontFamily="pressStart2P"
+        paddingLeft={[1, 2, 4]}
+      >
+        {children}
+      </ListItem>
+    ),
+    ol: ({ children }) => (
+      <OrderedList
+        w="full"
+        marginY={[1, 2, 4]}
+        spacing={[1, 2, 4]}
+        listStylePosition="inside"
+      >
+        {children}
+      </OrderedList>
+    ),
     p: ({ children }) => (
       <Text
         as="p"
@@ -88,107 +186,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       >
         {children}
       </UnorderedList>
-    ),
-    ol: ({ children }) => (
-      <OrderedList
-        w="full"
-        marginY={[1, 2, 4]}
-        spacing={[1, 2, 4]}
-        listStylePosition="inside"
-      >
-        {children}
-      </OrderedList>
-    ),
-    li: ({ children }) => (
-      <ListItem
-        as="li"
-        fontSize={["xs", "sm"]}
-        color="black"
-        fontFamily="pressStart2P"
-        paddingLeft={[1, 2, 4]}
-      >
-        {children}
-      </ListItem>
-    ),
-    a: ({ href, children }) => {
-      if (href) {
-        if (href.startsWith("/")) {
-          return (
-            <Link href={href}>
-              <Text as="span" textDecoration="underline">
-                {children}
-              </Text>
-            </Link>
-          );
-        }
-
-        return (
-          <Link href={href} target="_blank" rel="nofollow noopener noreferrer">
-            <Text as="span" textDecoration="underline">
-              {children}
-            </Text>
-          </Link>
-        );
-      }
-    },
-    code: ({ children, className, ...props }) => {
-      const isInline = !className;
-
-      if (isInline) {
-        return (
-          <Code
-            backgroundColor="black"
-            borderRadius={["lg"]}
-            fontSize={["xs", "sm"]}
-            color="#79c0ff"
-            paddingX={[1, 2, 3]}
-            paddingY={1}
-            fontFamily="pressStart2P"
-            {...props}
-          >
-            {children}
-          </Code>
-        );
-      }
-
-      // Let rehypeHighlight handle the other, larger code blocks
-      return (
-        <code className={className} {...props}>
-          {children}
-        </code>
-      );
-    },
-    img: async ({ src = "", alt = "", ...props }) => {
-      const imageSrc = src.startsWith("/images/posts/")
-        ? src
-        : `/images/posts/${src}`;
-      const { width, height } = await getImageDimensions(imageSrc);
-
-      return (
-        <Image
-          src={imageSrc}
-          alt={alt}
-          width={width}
-          height={height}
-          {...props}
-        />
-      );
-    },
-    blockquote: ({ children }) => (
-      <Box
-        as="blockquote"
-        borderLeft="4px solid #00cdac"
-        // borderColor="white"
-        pl={4}
-        py={2}
-        my={[2, 4]}
-        lineHeight={1.4}
-        bg="#f5f5f5"
-        fontStyle="italic"
-        color="#212529"
-      >
-        {children}
-      </Box>
     ),
     ...components,
   };
